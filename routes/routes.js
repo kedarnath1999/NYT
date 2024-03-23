c50e49b3792dafb8b86527552a15907b7a21f6d6
kedar <kedarnathgubbi.1999@gmail.com>
2024-03-23 17:14:05 -0400

    create basic home page layout using mostpopular API

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const axios = require('axios');
 const router = express.Router()
+const apiKey = process.env.API_KEY
 
 
-router.get('/',(req,res) => {
-    res.render("index")
+router.get('/', async (req,res) => {
+    try {
+        const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${apiKey}
+        `);
+        const articles = response.data.results;
+        res.render('index', { articles: articles });
+    } catch (error) {
+        console.error(error);
+        res.send('An error occurred');
+    }
 })
 
 router.get('/news',(req,res) => {
